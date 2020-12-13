33672013bd2fa877a37a66703df01b52a71965e6
Ssimoes48 <pbear48@aol.com>
2020-12-12 22:37:40 -0500

    working on append code

--- UFO_level_1/static/js/app.js
+++ UFO_level_1/static/js/app.js
@@ -3,11 +3,11 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
-// Use D3 to select the table body
-var tbody = d3.select("tbody");
-
 // Use D3 to select the table
 var table = d3.select("#ufo-table");
+
+// Use D3 to select the table body
+var tbody = d3.select("tbody");
 
 // Iterate through each student/grade pair
 tableData.forEach(([datetime, city, state, country, shape, durationMinutes, comments]) => {
@@ -29,3 +29,35 @@ tableData.forEach(([datetime, city, state, country, shape, durationMinutes, comm
 
 // Prevent the page from refreshing
 //   d3.event.preventDefault();
+
+
+
+
+// Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = time.filter(date => date.datetime === inputValue);
+
+  console.log(filteredData);
