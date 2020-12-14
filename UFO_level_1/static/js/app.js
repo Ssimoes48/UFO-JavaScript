728262352fdcad0c429f2fffa20b595a0db8472d
Ssimoes48 <pbear48@aol.com>
2020-12-13 21:38:56 -0500

    cleaned up code

--- UFO_level_1/static/js/app.js
+++ UFO_level_1/static/js/app.js
@@ -1,21 +1,11 @@
-// from data.js
 var sightings = data;
 
-// YOUR CODE HERE!
-
-// Use D3 to select the table
-var table = d3.select("#ufo-table");
-
-// Use D3 to select the table body
 var tbody = d3.select("tbody");
 
-// Iterate through each student/grade pair
 sightings.forEach(sighting => {
-
-    // Append one table row per student/grade
+ 
     var row = tbody.append("tr");
 
-    // append one cell for the student and one cell for the grade
     row.append("td").text(sighting.datetime);
     row.append("td").text(sighting.city);
     row.append("td").text(sighting.state);
@@ -26,24 +16,17 @@ sightings.forEach(sighting => {
 
 });
 
-
-// Select the button
 var button = d3.select("#filter-btn");
 
-// Select the form
 var form = d3.select("form");
 
-// Create event handlers 
 button.on("click", enterDate);
 form.on("submit", enterDate);
 
-// Complete the event handler function for the form
 function enterDate() {
 
-    // Prevent the page from refreshing
     d3.event.preventDefault();
 
-    // Select the input element and get the raw HTML node
     var inputDate = d3.select("#datetime").property("value");
 
     console.log(inputDate);
@@ -61,10 +44,8 @@ function enterDate() {
 
     filteredDate.forEach(sighting => {
 
-        // Append one table row per student/grade
         var row = tbody.append("tr");
     
-        // append one cell for the student and one cell for the grade
         row.append("td").text(sighting.datetime);
         row.append("td").text(sighting.city);
         row.append("td").text(sighting.state);
@@ -75,4 +56,3 @@ function enterDate() {
 
     });}
 
-    
